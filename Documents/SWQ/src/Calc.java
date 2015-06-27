c0e16f9bb409bec5c7fc00a5e734ab2b29c1a8a3
ys-nishio <yasuhiko.nishio@change-vision.com>
2015-06-27 20:24:40 +0900

    Javaソースコード管理

---
+++ Documents/SWQ/src/Calc.java
@@ -0,0 +1,7 @@
+class Calc{
+  int result;
+
+  void plus(int val1, int val2){
+    result = val1 + val2;
+  }
+}
---
+++ Documents/SWQ/src/Robot.java
@@ -0,0 +1,9 @@
+class Robot{
+  void hello(){
+    System.out.println("‚±‚ñ‚É‚¿‚Í");
+  }
+
+  void bye(){
+    System.out.println("‚³‚æ‚¤‚È‚ç");
+  }
+}
